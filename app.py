a390f4cece41b5b7371576c9cb837d83d8e77444
PrincessKaPa <94419616+PrincessKaPa@users.noreply.github.com>
2024-05-02 10:09:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ if img_file_buffer is not None:
     prediction = model.predict(data)
     print(prediction)
     if prediction[0][0]>0.3:
-      st.header('Abriendo')
-      client1.publish("IMIA","{'gesto': 'para'}",qos=0, retain=False)
+      st.header('para')
+      client1.publish("KIMIA","{'gesto': 'para'}",qos=0, retain=False)
       time.sleep(0.2)
     if prediction[0][1]>0.3:
-      st.header('Cerrando')
-      client1.publish("IMIA","{'gesto': 'check'}",qos=0, retain=False)
+      st.header('check')
+      client1.publish("KIMIA","{'gesto': 'check'}",qos=0, retain=False)
       time.sleep(0.2)  
